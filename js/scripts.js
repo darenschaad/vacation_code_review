955bd728d45a1822f123b35c2597a08c9367b010
Daren Schaad <darenschaad@gmail.com>
2016-01-15 08:59:48 -0800

    add variables to the scripts.js file for the 5 questions to ask the user

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var topical = "Would you like to go somewhere warm or tropical?";
+var touristy = "Would you like to go somewhere touristy?";
+var affordable = "Would you like your vacation to be affordable?"
+var fly = "Would you like to fly to your vacation spot?"
+var usa = "Would you like to remain in the USA for your vacation?"
+var currentQuestion = tropical;
